f3805fbe7576cfec30892b16470336e981b9a594
Metakar1 <patema@hermit.im>
2022-07-08 14:53:40 +0800

    Add disk driver

--- device/disk/disk.cxx
+++ device/disk/disk.cxx
@@ -1,30 +1,63 @@
 #include <xinu.h>
 
-extern "C"
-{
+extern "C" {
 
-devcall	diskread(
-	  struct dentry	*devptr,	/* Entry in device switch table	*/
-	  char	*buff,			/* Buffer of characters		*/
-	  int32	count 			/* Count of character to read	*/
-	)
-{
-    // TODO
+char   sector_buff[512];
+uint32 seek_offset;
+
+devcall diskread(struct dentry *devptr, /* Entry in device switch table	*/
+                 char          *buff,   /* Buffer of characters		*/
+                 int32          count   /* Count of character to read	*/
+) {
+	uint32 read_count = 0;
+	uint32 offset_in_sector;
+	uint32 sector;
+	while (read_count < count) {
+		offset_in_sector = seek_offset % 512;
+		sector = seek_offset / 512;
+
+		outb(0x1f3, sector & 0xff);
+		outb(0x1f4, (sector >> 8) & 0xff);
+		outb(0x1f5, (sector >> 16) & 0xff);
+		outb(0x1f6, 0xe0 | ((sector >> 24) & 0x0f));
+		outb(0x1f2, 1);
+		outb(0x1f7, 0x20);
+
+		while ((inb(0x1f7) & 8) == 0) {};
+		insw(0x1f0, (int32)sector_buff, 256);
+
+		while (offset_in_sector < 512 && read_count < count) {
+			buff[read_count] = sector_buff[offset_in_sector];
+			read_count++;
+			offset_in_sector++;
+			seek_offset++;
+		}
+	}
+	return OK;
 }
 
-devcall	diskgetc(
-	  struct dentry	*devptr		/* Entry in device switch table	*/
-    )
-{
-    // TODO
+devcall diskgetc(struct dentry *devptr /* Entry in device switch table	*/
+) {
+	int32 offset_in_sector = seek_offset % 512;
+	int32 sector = seek_offset / 512;
+
+	outb(0x1f3, sector & 0xff);
+	outb(0x1f4, (sector >> 8) & 0xff);
+	outb(0x1f5, (sector >> 16) & 0xff);
+	outb(0x1f6, 0xe0 | ((sector >> 24) & 0x0f));
+	outb(0x1f2, 1);
+	outb(0x1f7, 0x20);
+
+	while ((inb(0x1f7) & 8) == 0) {};
+	insw(0x1f0, (int32)sector_buff, 256);
+	seek_offset++;
+	return sector_buff[offset_in_sector];
 }
 
-devcall	diskseek (
-	  struct dentry *devptr,	/* Entry in device switch table */
-	  uint32	offset		/* Byte position in the file	*/
-	)
-{
-    // TODO
+devcall diskseek(struct dentry *devptr, /* Entry in device switch table */
+                 uint32         offset  /* Byte position in the file	*/
+) {
+	seek_offset = offset;
+	return OK;
 }
-
 }
--- system/main.c
+++ system/main.c
@@ -1,11 +1,15 @@
 /*  main.c  - main */
 
 #include <xinu.h>
+#include "filesystem.h"
 
-process	main(void)
-{
-
+process main(void) {
 	/* Run the Xinu shell */
+
+	char elf[8192];
+	read_file("HELLO.ELF", elf, 8192);
+	uint32 offset = get_elf_entrypoint(elf);
+	resume(create(elf + offset, INITSTK, 60, "hello.elf", 1, kprintf));
 
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
@@ -19,5 +23,4 @@ process	main(void)
 		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
 	}
 	return OK;
-    
 }
